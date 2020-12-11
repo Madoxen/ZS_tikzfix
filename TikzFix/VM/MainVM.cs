0f617e735ad68ccf5d2c0004ec4fbd2dc7ca8f89
Paweł Chłąd <pchlad5@gmail.com>
2020-12-11 20:02:48 +0000

    Canvas collection drawing (#1)
    * Finished work on exposing Children property from Canvas
    
    * Resolved exception throw on cast
    
    * Removed test button

--- TikzFix/VM/MainVM.cs
+++ TikzFix/VM/MainVM.cs
@@ -1,22 +1,42 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Shapes;
 
 namespace TikzFix.VM
 {
     class MainVM : BaseVM
     {
-        private ObservableCollection<Shape> shapes;
-        public ObservableCollection<Shape> Shapes
+        private ObservableCollection<Shape> shapes = new ObservableCollection<Shape>();
+        public ICollection<Shape> Shapes
         {
             get { return shapes; }
-            private set { SetProperty(ref shapes, value); }
+           // private set { SetProperty<ICollection<Shape>>(ref shapes, value); }
         }
 
-
+        public MainVM()
+        {
+            // Add a Line Element
+            Line myLine = new Line
+            {
+                Stroke = System.Windows.Media.Brushes.LightSteelBlue,
+                X1 = 1,
+                X2 = 50,
+                Y1 = 1,
+                Y2 = 50,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Center,
+                StrokeThickness = 2
+            };
+            Shapes.Add(myLine);
+        }
     }
 }
--- TikzFix/Views/CollectionCanvas.xaml.cs
+++ TikzFix/Views/CollectionCanvas.xaml.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+
 using System.Windows.Shapes;
 
 namespace TikzFix.Views
@@ -23,11 +18,105 @@ namespace TikzFix.Views
         public CollectionCanvas()
         {
             InitializeComponent();
-            
         }
 
-        
+        public ICollection<Shape> Shapes
+        {
+            get { return (ICollection<Shape>)GetValue(ShapesProperty); }
+            set { SetValue(ShapesProperty, value); }
+        }
 
-        
+        // Using a DependencyProperty as the backing store for Children.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShapesProperty =
+            DependencyProperty.Register("Shapes", typeof(ICollection<Shape>), typeof(CollectionCanvas), new PropertyMetadata(null, OnShapesPropertyChanged));
+
+
+
+
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(CollectionCanvas), new PropertyMetadata(0));
+
+
+
+        //Called when SetProperty is called in VM
+        //Or otherwise INotifyPropertyChanged
+        //If set to null, will simply Clear the collection
+        private static void OnShapesPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is CollectionCanvas collectionCanvas)
+            {
+                if (e.NewValue is ICollection<Shape> collection)
+                {
+                    collectionCanvas.c.Children.Clear(); //clear all children
+                    foreach (Shape element in collection)
+                    {
+                        collectionCanvas.c.Children.Add(element);
+                    }
+
+                    if (collection is INotifyCollectionChanged cc)
+                    {
+                        Debug.WriteLine("ASDASADAS");
+                        cc.CollectionChanged += collectionCanvas.CollectionChanged;
+                    }
+
+                    collectionCanvas.Shapes = collection;
+                }
+                else if (e.NewValue == null)
+                {
+                    collectionCanvas.c.Children.Clear();
+                    collectionCanvas.Shapes = null;
+                }
+                else
+                {
+                    throw new ArgumentException("Value type mismatch: is " + e.NewValue.GetType().Name + "required ICollection<Shape>");
+                }
+
+                if (e.OldValue is INotifyCollectionChanged oldcc)
+                {
+                    oldcc.CollectionChanged -= collectionCanvas.CollectionChanged;
+                }
+            }
+        }
+
+        private void CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            var newItems = e.NewItems?.Cast<Shape>().ToList();
+            var oldItems = e.OldItems?.Cast<Shape>().ToList();
+       
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (Shape ele in newItems)
+                    {
+                        c.Children.Add(ele);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (Shape ele in oldItems)
+                    {
+                        c.Children.Remove(ele);
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    c.Children[e.OldStartingIndex] = (Shape)newItems.ElementAt(0); //TODO: not sure if this works in all cases?
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                    throw new NotImplementedException();
+                    break;
+            }
+        }
     }
+
+
+
 }
