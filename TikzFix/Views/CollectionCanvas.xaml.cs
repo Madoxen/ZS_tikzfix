8952b623e430fbd9654e3082a9dd9c83593f13d0
Madoxen <pchlad5@gmail.com>
2020-12-11 18:05:07 +0100

    Added base MVVM classes

--- TikzFix/Views/CollectionCanvas.xaml.cs
+++ TikzFix/Views/CollectionCanvas.xaml.cs
@@ -13,20 +13,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TikzFix
+namespace TikzFix.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CollectionCanvas.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CollectionCanvas : UserControl
     {
-        public MainWindow()
+        public CollectionCanvas()
         {
             InitializeComponent();
-
             
-
-
         }
+
+        
+
+        
     }
 }
