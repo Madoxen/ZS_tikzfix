44f713f6bc83bd7a55227d4d8c14a3ff6f429afa
CzarqR <czarekr98@gmail.com>
2020-12-17 13:20:22 +0100

    Tikz mirrored vertical

--- TikzFix/Model/Tool/ITool.cs
+++ TikzFix/Model/Tool/ITool.cs
@@ -4,7 +4,8 @@ namespace TikzFix.Model.Tool
 {
     interface ITool
     {
-        public const string TIKZ_MAIN = "\\begin{{tikzpicture}}[xscale=0.1, yscale=0.1]\n{0}\n\\end{{tikzpicture}}";
+        // todo, change x/yscale to 1 and scale shape to fill generated drawing
+        public const string TIKZ_MAIN = "\\begin{{tikzpicture}}[xscale=0.1, yscale=-0.1]\n{0}\n\\end{{tikzpicture}}"; // y scale has to be negative, tikz is mirrored vertically
 
         DrawingShape GetShape(CanvasEventArgs canvasEventArgs);
 
