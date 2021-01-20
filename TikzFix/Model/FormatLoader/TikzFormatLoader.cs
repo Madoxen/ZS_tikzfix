91adf55e3a031dc8487617122af637836b4ec880
Madoxen <pchlad5@gmail.com>
2021-01-20 09:57:06 +0100

    SVG to TikzShape

--- TikzFix/Model/FormatLoader/TikzFormatLoader.cs
+++ TikzFix/Model/FormatLoader/TikzFormatLoader.cs
@@ -6,8 +6,11 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+using System.Windows.Media;
 using TikzFix.Model.TikzShapes;
+using TikzFix.Utils;
+using TikzFix.Model.Styling;
+using System.Windows.Ink;
 
 namespace TikzFix.Model.FormatLoader
 {
@@ -17,7 +20,10 @@ namespace TikzFix.Model.FormatLoader
         const string BEGIN_ENVIROMENT_TOKEN = "\\begin{tikzpicture}";
         const string END_ENVIROMENT_TOKEN = "\\end{tikzpicture}";
         const string bins = @"texmfs\install\miktex\bin\x64";
-        
+        Regex[] svgRegexes = new Regex[]{
+          new Regex(@"<path.*"), //path line
+         new Regex(@"d='(.*?)'"), //data 
+    };
 
 
         public ICollection<TikzShape> ConvertMany(string data)
@@ -26,8 +32,6 @@ namespace TikzFix.Model.FormatLoader
             {
                 //1. find tizkpicture enviroment
                 string env = FindEnviroment(data);
-
-
 
                 //2. Create document with standalone package
                 string doc = @"
@@ -39,7 +43,7 @@ namespace TikzFix.Model.FormatLoader
 
                 File.WriteAllText("job.txt", doc);
 
-                //3. Call latex for conversion
+                //3. Call latex for conversion to DVI
                 Process latex = new Process();
                 latex.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
                 latex.StartInfo.FileName = "latex";
@@ -53,7 +57,6 @@ namespace TikzFix.Model.FormatLoader
                     throw new Exception("Latex conversion failed");
 
                 //4. Call dvisvgm to convert dvi to svg that we can easily parse
-
                 Process dvi2svg = new Process();
                 dvi2svg.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
                 dvi2svg.StartInfo.FileName = "dvisvgm";
@@ -67,18 +70,116 @@ namespace TikzFix.Model.FormatLoader
                     throw new Exception("DVI conversion failed");
 
                 string svg = File.ReadAllText("job.svg");
-                Debug.WriteLine(svg);
+         
 
-                //5. Parse SVG to tikzShapes
-                return new List<TikzShape>{ }; 
+
+                MatchCollection matches = svgRegexes[0].Matches(svg);
+                List<TikzShape> result = new List<TikzShape>();
+
+                foreach (Match m in matches)
+                {
+                    string line = m.Value;
+                    string d = svgRegexes[1].Match(line).Groups[1].Value;
+                    System.Windows.Shapes.Path p = new System.Windows.Shapes.Path();
+                    p.Data = Geometry.Parse(d);
+                    //Parse style
+
+
+                    string rawStrokeColor = GetSVGProperty(line, "stroke");
+                    string rawFillColor = GetSVGProperty(line, "fill");
+                    string rawStrokeWidth = GetSVGProperty(line, "stroke-width");
+                    string rawStrokeDasharray = GetSVGProperty(line, "stroke-dasharray");
+                    string rawStrokeMiterlimit = GetSVGProperty(line, "stroke-miterlimit");
+                    string rawStrokeOpacity = GetSVGProperty(line, "stroke-opacity");
+                    string rawFillOpacity = GetSVGProperty(line, "fill-opacity");
+
+                    
+                    
+                    double strokeOpacity = double.TryParse(rawStrokeOpacity, out double o) ? o : 1.0;
+                    double fillOpacity = double.TryParse(rawFillOpacity, out double of) ? of : 1.0;
+                    TikzStyle s = new TikzStyle(parseSVGColor(rawStrokeColor, strokeOpacity),
+                        parseSVGColor(rawFillColor, fillOpacity), LineEnding.NONE, praseSVGStrokeWidth(rawStrokeWidth), parseSVGDashArray(rawStrokeDasharray));
+                    p.SetStyle(s);
+
+                    Debug.WriteLine(rawStrokeColor);
+                    result.Add(new TikzPath(p, s, d));
+                }
+                return result;
             }
-            catch (Exception e) {
-                Debug.WriteLine(e.Message);
-                return new List<TikzShape>{ }; 
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return new List<TikzShape> { };
             }
         }
 
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="property"></param>
+        /// <returns>property data or empty if property does not exist</returns>
+        private string GetSVGProperty(string data, string property)
+        {
+            try
+            {
+                return Regex.Match(data, property + "='(.*?)'").Groups[1].Value;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+
+
+        //Parses SVG hex color
+        private Color parseSVGColor(string rawColor, double opacity = 1.0)
+        {
+            if (rawColor == "none" || String.IsNullOrEmpty(rawColor))
+                return Colors.Transparent;
+            Color c = (Color)ColorConverter.ConvertFromString(rawColor);
+            Debug.WriteLine(opacity * 255);
+            return Color.FromArgb((byte)Math.Round(opacity * 255), c.R, c.G, c.B);
+        }
+
+        private LineWidth praseSVGStrokeWidth(string rawWidth)
+        {
+            if (string.IsNullOrEmpty(rawWidth))
+                return LineWidth.THIN;
+
+            double val = Double.Parse(rawWidth);
+            if (val <= .19926)
+                return LineWidth.VERY_THIN;
+            if (val <= .3986)
+                return LineWidth.THIN;
+            if (val <= .79702)
+                return LineWidth.THICK;
+            return LineWidth.ULTRA_THICK;
+        }
+
+        private LineType parseSVGDashArray(string rawDashArray)
+        {
+            //Dotted -> first value different then second value
+            //Dashed -> first and second values are the same
+            //Dashed-Dotted -> 4 or more values
+            if (string.IsNullOrEmpty(rawDashArray))
+                return LineType.SOLID;
+
+            double[] values = rawDashArray.Split(',').Select(x => Double.Parse(x)).ToArray();
+            if (values.Length > 2)
+                return LineType.DASHDOTTED;
+
+            if (values[0] != values[1])
+                return LineType.DOTTED;
+
+            if (values[0] == values[1])
+                return LineType.DASHED;
+
+            return LineType.SOLID;
+
+        }
 
         //Returns enviroment (text block) specified by BEGIN_ENVIROMENT_TOKEN and END_ENVIROMENT_TOKEN
         public string FindEnviroment(string data)
@@ -114,10 +215,9 @@ namespace TikzFix.Model.FormatLoader
                 }
             }
 
-            
             return data[env_start..(last_token)];
         }
 
-        
+
     }
 }
---
+++ TikzFix/Model/TikzShapes/TikzPath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+using TikzFix.Model.Styling;
+using TikzFix.Model.Tool;
+
+namespace TikzFix.Model.TikzShapes
+{
+    class TikzPath : TikzShape
+    {
+        public TikzPath(Path path, TikzStyle style, string rawData) : base(path, style) {
+            this.path = path;
+            this.rawData = rawData;
+        }
+
+        private string rawData;
+        private Path path;
+        public override Shape Shape {
+            get => path;
+            set
+            {
+                if (value is not Path p)
+                {
+                    throw new ArgumentException("TikzLine Shape has to be Line");
+                }
+                path = p;
+            }
+        }
+
+        public override LocalShapeData GenerateLocalData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GenerateTikz()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
